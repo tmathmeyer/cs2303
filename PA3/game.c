0360c6b8c8cd7b3202328b0abc8352ab207a62c4
Ted Meyer <tmathmeyer@gmail.com>
2013-09-13 09:52:22 -0400

    sadsda

--- PA3/game.c
+++ PA3/game.c
@@ -20,11 +20,10 @@ int main(int argc, char*argv[])
 
 	// init the arrays
 	int *grid_a = read_file(argv[4], width, height);
-	int* grid_b[width*height];
-	int* grid_c[width*height];
+	int grid_b[width*height];
+	int grid_c[width*height];
 	memset(grid_c, 0, width*height);
 	memset(grid_b, 0, width*height);
-	memset(grid_a, 0, width*height);
 
 	
 	run_iterations(grid_a, grid_b, generations, print, pause);
