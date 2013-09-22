5ce5c2b8448571611cf3a54f2927cfcf52bbc7c8
Ted Meyer <tmathmeyer@gmail.com>
2013-09-21 20:43:59 -0400

    oh god the fuck ups

--- PA2/words.c
+++ PA2/words.c
@@ -36,16 +36,37 @@ int main(int argc, char*argv[])
 	return 0;
 }
 
+int read_words(char *filename, char *words[], int max_count)
+{
+	//init variables
+	FILE *f = fopen(filename, "rt");
+	int i = 0;
+	char temp[100];
+	//scan through the file
+	while (fscanf(f, "%s", temp) != -1)
+	{
+		words[i] = strdup(temp);
+		i++;
+	}
+	//close the file and exit
+	fclose(f);
+	return i;
+}
+
+
+
 void fillTree(struct node **tree, char *filename)
 {
-	FILE *f = fopen(filename, "rt");
-	char temp[100]; //word length max is 100.
-	while(fscanf(f, "%s", temp) != -1)
+	//iniv cariables *no more than 20000 words per file*
+	char *words[200000];
+	int count = read_words(filename, words, 200000);
+	int i;
+	//read words, trim them, and insert them into the tree
+	for (i = 0; i < count; i++)
 	{
-		remove_punctuation(temp);
-		insert(temp, tree);
+		remove_punctuation(words[i]);
+		insert(words[i], tree);
 	}
-	fclose(f);
 }
 
 
